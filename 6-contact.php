ce66d316e3cf8b20effbf51b44e6e4489983a95c
Alexandre Vezina <avezina@ubikvoip.com>
2017-09-09 14:45:25 -0400

    Updated contact info

--- 6-contact.php
+++ 6-contact.php
@@ -2,28 +2,16 @@
 	<h2>Contact</h2>
 
 	<div class="contact-elem">
-        <h3>Booking</h3>
-        <div>
-            <span class="email">
-                <a href="mailto:pauline@outloud.mu">Pauline Viard</a>
-            </span>
-        </div>
-        <div class="phone">(438) 921-3229</div>
-	</div>
-
-	<div class="contact-elem">
 		<h3>Relations de presse et promotion radio</h3>
 		<div>
 			<span class="email">
-				<a href="mailto:lamozik@videotron.ca">Jean Lamothe - lamozik@videotron.ca</a>
+				<a href="mailto:alex@doloreanne.com">Alexandre V&eacute;zina</a>
 			</span>
 		</div>
-
-		<div class="phone">(514) 594-9835</div>
 	</div>
 
 	<div class="contact-elem">
-		<h3>Général</h3>
+		<h3>Booking</h3>
 		<div>
 			<span class="email">
 				<a href="mailto:info@doloreanne.com">info@doloreanne.com</a>
--- media/6-contact.php
+++ media/6-contact.php
@@ -5,11 +5,11 @@
 		<h3>Relations de presse et promotion radio</h3>
 		<div>
 			<span class="email">
-				<a href="mailto:lamozik@videotron.ca">Jean Lamothe</a>
+				<a href="mailto:alex@doloreanne.com">Alexandre V&eacute;zina</a>
 			</span>
 		</div>
 
-		<div class="phone">(514) 594-9835</div>
+		<div class="phone">(581) 982-5190</div>
 	</div>
 
 	<div class="follow">
@@ -18,27 +18,27 @@
 			<div class="button-container">
 				<div class="button first">
 					<a href="http://facebook.com/doloreanne" target="BLANK">
-						<img id="facebook-button" src="./img/facebook.png" />
+						<img id="facebook-button" src="../img/facebook.png" />
 					</a>
 				</div>
 				<div class="button">
 					<a href="https://www.youtube.com/user/groupedoloreanne/videos" target="BLANK">
-						<img style="padding-left: 9px;" id="youtube-button" src="./img/youtube.png" />
+						<img style="padding-left: 9px;" id="youtube-button" src="../img/youtube.png" />
 					</a>
 				</div>
 				<div class="button">
 					<a href="http://doloreanne.bandcamp.com" target="BLANK">
-						<img style="padding-left: 3px;" id="bandcamp-button" src="./img/bandcamp.png" />
+						<img style="padding-left: 3px;" id="bandcamp-button" src="../img/bandcamp.png" />
 					</a>
 				</div>
 				<div class="button">
 					<a href="http://twitter.com/doloreanne" target="BLANK">
-						<img style="padding-left: 9px;" id="twitter-button" src="./img/twitter.png" />
+						<img style="padding-left: 9px;" id="twitter-button" src="../img/twitter.png" />
 					</a>
 				</div>
 				<div class="button">
 					<a href="https://plus.google.com/+DoloreanneMusique/" target="BLANK">
-						<img style="padding-left: 9px;" id="gplus-button" src="./img/gplus.png" />
+						<img style="padding-left: 9px;" id="gplus-button" src="../img/gplus.png" />
 					</a>
 				</div>
 				<div style="height:0;width:0;clear:both;">&nbsp;</div>
@@ -49,6 +49,6 @@
 	<div style="clear:both;height:0;width:0;">&nbsp;</div>
 
 	<p style="text-align: center; margin-top: 5em;">
-		<img src="/img/wezo.svg" alt="LOGO" />
+		<img src="../img/wezo.svg" alt="LOGO" />
 	</p>
 </div>
