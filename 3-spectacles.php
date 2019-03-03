37c0426453113d20bcd185be3c13737361d7a689
Alexandre Vezina <avezina@ubikvoip.com>
2019-03-02 20:52:47 -0500

    Test GitHub pages

--- 3-spectacles.php
+++
@@ -1,7 +0,0 @@
-<div class="section" style="" id="spectacles">
-
-	<h2>Spectacles</h2>
-	<div style="padding:0 100px;">
-		<script charset="utf-8" src="https://widget.bandsintown.com/main.min.js"></script><a class="bit-widget-initializer" data-artist-name="Doloréanne" data-display-local-dates="false" data-display-past-dates="true" data-auto-style="false" data-text-color="#2b3175" data-link-color="#2b3175" data-popup-background-color="#fbd63c" data-background-color="#fbd63c" data-display-limit="15" data-link-text-color="#fbd63c"></a>
-	</div>
-</div>
